e81d45b3fde7d0a69082483cc66a8857e5ce3693
johnnyxcy <johnny.xcy1997@gmail.com>
2019-04-13 16:11:21 -0700

    update browser_action and click tester

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,1 @@
 'use strict';
-
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-  });
