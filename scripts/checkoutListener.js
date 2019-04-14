cd3b19bd973e0e6014e49920e007a68344ef418c
leontaolong <longt8@uw.edu>
2019-04-13 17:56:02 -0700

    Refactor checkoutListener

--- scripts/checkoutListener.js
+++ scripts/checkoutListener.js
@@ -1,38 +1,36 @@
-KEYWORDS = {
-    singleWords: [
-        'payment method', 
-        'CVV number', 
-        'CVV code',
-        'card number'
-    ],
-    platforms: [
-        'https://www.paypalobjects.com/',
-        'https://checkout.stripe.com/',
-    ],
-    matchers: [
-        'checkout',
-        'credit card',
-        'creditcard',
-        'billing',
-        'place order',
-        'pay',
-        'visa/mastercard',
-        'visa / mastercard',
-        'visa mastercard',
-        'card number',
-        'debit',
-    ]
-}
-
-
 $(() => {
-    let checkers = KEYWORDS.singleWords
-        .concat(KEYWORDS.platforms)
-        .concat(KEYWORDS.matchers);
-    let found = checkers.any((keyword) => { 
-        console.log($(`*:Contains(${keyword})`).length)
-        console.log(keyword)
-        return $(`*:Contains(${keyword})`).length}
+    const KEYWORDS = {
+        singleWords: [
+            'payment method', 
+            'CVV number', 
+            'CVV code',
+            'card number'
+        ],
+        platforms: [
+            'https://www.paypalobjects.com/',
+            'https://checkout.stripe.com/',
+        ],
+        matchers: [
+            'checkout',
+            'credit card',
+            'creditcard',
+            'billing',
+            'place order',
+            'pay',
+            'visa/mastercard',
+            'visa / mastercard',
+            'visa mastercard',
+            'card number',
+            'debit',
+        ]
+    };
+
+    let checkers = [];
+    Object.keys(KEYWORDS).forEach(keywords => {
+        checkers = checkers.concat(KEYWORDS[keywords]);
+    });
+    const found = checkers.any(keyword => 
+        $(`*:Contains(${keyword})`).length
     );
     console.log(found);
 });
