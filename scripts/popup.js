e6b059f403e1c8f381c35c24bd10b9c550040a99
johnnyxcy <johnny.xcy1997@gmail.com>
2019-04-13 21:36:09 -0700

    fix: checkbox

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,15 +3,15 @@
 window.onload = function() {
     var if_use = document.querySelector("input")
     if (if_use.checked) {
-        chrome.runtime.sendMessage({using: "checked"});
+        chrome.runtime.sendMessage({text: "check"});
     } else {
-        chrome.runtime.sendMessage({using: "not_checked"});
+        chrome.runtime.sendMessage({text: "uncheck"});
     }
     if_use.onchange = function() {
         if (if_use.checked) {
-            chrome.runtime.sendMessage({using: "checked"});
+            chrome.runtime.sendMessage({text: "check"});
         } else {
-            chrome.runtime.sendMessage({using: "not_checked"});
+            chrome.runtime.sendMessage({text: "uncheck"});
         }
     }
 }
